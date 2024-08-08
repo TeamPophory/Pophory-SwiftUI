ab375fcea1d845f1ca4cdf54e51cccfba32001a0
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-08 11:39:19 +0900

    [Chore] #1 - 프로젝트 생성

---
+++ pophory-TCA/pophory-TCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pophory-TCA
+//
+//  Created by 강윤서 on 8/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pophory-TCA/pophory-TCA/pophory_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  pophory_TCAApp.swift
+//  pophory-TCA
+//
+//  Created by 강윤서 on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct pophory_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
