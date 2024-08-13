570e340a2f11302bdfd5bc813d23691318edee83
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-13 11:25:06 +0900

    [Feat] #12 - view extension 함수 호출하여 Modifier 사용

--- pophory-TCA/pophory-TCA/Global/Extensions/View+.swift
+++ pophory-TCA/pophory-TCA/Global/Extensions/View+.swift
@@ -5,4 +5,12 @@
 //  Created by 강윤서 on 8/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+	func fontWithLineHeightView(fontType: UIFontWithLineHeight) -> some View {
+		return ModifiedContent(
+			content: self,
+			modifier: FontWithLineHeightViewModifier(font: fontType.font, lineHeight: fontType.lineHeight))
+	}
+}
