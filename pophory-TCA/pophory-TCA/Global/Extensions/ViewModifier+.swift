719972049c410ad64aa45e1ddfb4ad238f17c835
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-13 11:22:45 +0900

    [Feat] #12 - ViewModifier 함수 생성

--- pophory-TCA/pophory-TCA/Global/Extensions/ViewModifier+.swift
+++ pophory-TCA/pophory-TCA/Global/Extensions/ViewModifier+.swift
@@ -5,4 +5,16 @@
 //  Created by 강윤서 on 8/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct FontWithLineHeightViewModifier: ViewModifier {
+	let font: UIFont
+	let lineHeight: CGFloat
+	
+	func body(content: Content) -> some View {
+		content
+			.font(Font(font))
+			.lineSpacing(lineHeight - font.lineHeight)
+			.padding(.vertical, (lineHeight - font.lineHeight)/2)
+	}
+}
