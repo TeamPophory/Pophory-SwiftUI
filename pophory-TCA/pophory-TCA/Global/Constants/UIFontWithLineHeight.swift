a0fca77aa51f3a09feddc05df840ec7e8c979cd6
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-28 11:31:37 +0900

    [Fix] #14 - font 크기 수정

--- pophory-TCA/pophory-TCA/Global/Constants/UIFontWithLineHeight.swift
+++ pophory-TCA/pophory-TCA/Global/Constants/UIFontWithLineHeight.swift
@@ -27,21 +27,21 @@ extension UIFontWithLineHeight {
 	static let modalTitleBold: UIFontWithLineHeight = customPretendard(.Bold, size: 20, lineHeight: 28)
 	
 	// SemiBold
-	static let headline02SemiBold: UIFontWithLineHeight = customPretendard(.Bold, size: 20, lineHeight: 24)
-	static let headline03SemiBold: UIFontWithLineHeight = customPretendard(.Bold, size: 18, lineHeight: 17)
+	static let headline02SemiBold: UIFontWithLineHeight = customPretendard(.SemiBold, size: 20, lineHeight: 24)
+	static let headline03SemiBold: UIFontWithLineHeight = customPretendard(.SemiBold, size: 18, lineHeight: 17)
 	
 	// Medium
-	static let headline01Medium: UIFontWithLineHeight = customPretendard(.Bold, size: 24, lineHeight: 34)
-	static let title01Medium: UIFontWithLineHeight = customPretendard(.Bold, size: 17, lineHeight: 24)
-	static let text01Medium: UIFontWithLineHeight = customPretendard(.Bold, size: 16, lineHeight: 21)
-	static let caption01Medium: UIFontWithLineHeight = customPretendard(.Bold, size: 14, lineHeight: 20)
-	static let caption02Medium: UIFontWithLineHeight = customPretendard(.Bold, size: 12, lineHeight: 17)
-	static let popupButtonMedium: UIFontWithLineHeight = customPretendard(.Bold, size: 16, lineHeight: 23)
-	static let popupLineMedium: UIFontWithLineHeight = customPretendard(.Bold, size: 14, lineHeight: 17)
-	static let modalText: UIFontWithLineHeight = customPretendard(.Bold, size: 18, lineHeight: 26)
+	static let headline01Medium: UIFontWithLineHeight = customPretendard(.Medium, size: 24, lineHeight: 34)
+	static let title01Medium: UIFontWithLineHeight = customPretendard(.Medium, size: 17, lineHeight: 24)
+	static let text01Medium: UIFontWithLineHeight = customPretendard(.Medium, size: 16, lineHeight: 21)
+	static let caption01Medium: UIFontWithLineHeight = customPretendard(.Medium, size: 14, lineHeight: 20)
+	static let caption02Medium: UIFontWithLineHeight = customPretendard(.Medium, size: 12, lineHeight: 17)
+	static let popupButtonMedium: UIFontWithLineHeight = customPretendard(.Medium, size: 16, lineHeight: 23)
+	static let popupLineMedium: UIFontWithLineHeight = customPretendard(.Medium, size: 14, lineHeight: 17)
+	static let modalText: UIFontWithLineHeight = customPretendard(.Medium, size: 18, lineHeight: 26)
 	
 	// Regular
-	static let popupTextRegular: UIFontWithLineHeight = customPretendard(.Bold, size: 16, lineHeight: 23)
+	static let popupTextRegular: UIFontWithLineHeight = customPretendard(.Regular, size: 16, lineHeight: 23)
 }
 
 // static func
