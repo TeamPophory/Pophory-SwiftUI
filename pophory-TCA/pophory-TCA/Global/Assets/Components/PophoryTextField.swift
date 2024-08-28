127c6f87736efe076389b584b38f939b5357cf1d
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-28 11:31:37 +0900

    [Fix] #14 - font 크기 수정

---
+++ pophory-TCA/pophory-TCA/Global/Assets/Components/PophoryTextField.swift
@@ -0,0 +1,18 @@
+//
+//  PophoryTextField.swift
+//  pophory-TCA
+//
+//  Created by 강윤서 on 8/27/24.
+//
+
+import SwiftUI
+
+struct PophoryTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PophoryTextField()
+}
