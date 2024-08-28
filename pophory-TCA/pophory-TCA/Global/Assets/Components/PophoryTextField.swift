e00ca969e24ff26c2d7b2096198304311f1e7cda
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-28 11:32:21 +0900

    [Feat] #14 - TextFieldStyle을 사용한 custom textfield

--- pophory-TCA/pophory-TCA/Global/Assets/Components/PophoryTextField.swift
+++ pophory-TCA/pophory-TCA/Global/Assets/Components/PophoryTextField.swift
@@ -7,9 +7,27 @@
 
 import SwiftUI
 
+struct PophoryTextFieldStyle: TextFieldStyle {
+	func _body(configuration: TextField<Self._Label>) -> some View {
+		configuration
+			.padding(EdgeInsets(top: 19, leading: 15, bottom: 18, trailing: 18))
+			.fontWithLineHeightView(fontType: .text01Medium)
+			.overlay(
+				RoundedRectangle(cornerRadius: 18)
+					.stroke(.pryPurple, lineWidth: 1.0)
+			)
+			.onAppear {
+				UITextField.appearance().clearButtonMode = .whileEditing
+			}
+	}
+}
+
 struct PophoryTextField: View {
+	@State var text = ""
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("Hi...", text: $text)
+			.textFieldStyle(PophoryTextFieldStyle())
     }
 }
 
