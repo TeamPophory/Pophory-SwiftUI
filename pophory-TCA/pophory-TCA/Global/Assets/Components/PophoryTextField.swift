e562c6000d4779b56075816d5eaecd9f3ae9c968
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-08-30 12:20:16 +0900

    [Feat] #14 - state를 사용하여 상태변경에 따른 stroke 색상 변경

--- pophory-TCA/pophory-TCA/Global/Assets/Components/PophoryTextField.swift
+++ pophory-TCA/pophory-TCA/Global/Assets/Components/PophoryTextField.swift
@@ -8,13 +8,15 @@
 import SwiftUI
 
 struct PophoryTextFieldStyle: TextFieldStyle {
+	@Binding var isEmpty: Bool
+	
 	func _body(configuration: TextField<Self._Label>) -> some View {
 		configuration
 			.padding(EdgeInsets(top: 19, leading: 15, bottom: 18, trailing: 18))
 			.fontWithLineHeightView(fontType: .text01Medium)
 			.overlay(
 				RoundedRectangle(cornerRadius: 18)
-					.stroke(.pryPurple, lineWidth: 1.0)
+					.stroke(isEmpty ? .gray300 : .pryPurple, lineWidth: 1.0)
 			)
 			.onAppear {
 				UITextField.appearance().clearButtonMode = .whileEditing
@@ -23,14 +25,16 @@ struct PophoryTextFieldStyle: TextFieldStyle {
 }
 
 struct PophoryTextField: View {
-	@State var text = ""
+	@State private var text: String = ""
+	let placeholder: String
 	
-    var body: some View {
-        TextField("Hi...", text: $text)
-			.textFieldStyle(PophoryTextFieldStyle())
-    }
+	var body: some View {
+		TextField(placeholder, text: $text)
+			.textFieldStyle(PophoryTextFieldStyle(isEmpty: .constant(text.isEmpty))
+		)
+	}
 }
 
 #Preview {
-    PophoryTextField()
+	PophoryTextField(placeholder: "Enter")
 }
